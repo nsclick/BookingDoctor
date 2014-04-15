a0ee714debdaac3bd0b723deff86c8bd026305ab
API reservas Dávila <bookingdoctor@nsberraco.local>
2014-04-15 14:34:03 -0300

    Cambio de entorno de desarrollo

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,14 +8,14 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="<?php echo base_url("home");?>">Reserva de Horas</a>
+          <a class="navbar-brand" href="<?php echo site_url("home");?>">Reserva de Horas</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li><a href="<?php echo base_url("consulta/");?>">Consulta de horas</a></li>
-            <li><a href="<?php echo base_url("anulacion/");?>">Anulaci&oacute;n de horas</a></li>
-            <li><a href="<?php echo base_url("modificardatos/");?>">Modificar datos</a></li>
-            <li><a href="<?php echo base_url("registro/");?>">Registro</a></li>
+            <li><a href="<?php echo site_url("consulta/");?>">Consulta de horas</a></li>
+            <li><a href="<?php echo site_url("anulacion/");?>">Anulaci&oacute;n de horas</a></li>
+            <li><a href="<?php echo site_url("modificardatos/");?>">Modificar datos</a></li>
+            <li><a href="<?php echo site_url("registro/");?>">Registro</a></li>
             
           </ul>
         </div><!--/.nav-collapse -->
