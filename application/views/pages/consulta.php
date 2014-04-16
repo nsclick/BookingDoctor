98f9cdf0bc3b1cf074ae50cbe42097096c83eb9c
Ricardo Escobar <zherethor@gmail.com>
2014-04-16 18:26:52 -0300

    Creación página consulta de horas
    más edición de css

--- application/views/pages/consulta.php
+++ application/views/pages/consulta.php
@@ -1,1 +1,36 @@
-HOLA MUNDO
+<div class="consulta">
+	<h1>Consulta de Horas</h1>
+	<p>Aqu&iacute; podr&aacute; ver las horas solicitadas. Las que a&uacute;n se encuentren vigentes podr&aacute;n ser anuladas.</p>
+	<p class="hora">
+		<span>Nombre: </span>MORIAL MARQUEZ CHANAL
+		<br />
+		<span>Código Reserva: </span>  11372248
+		<br />
+		<span>Fecha: </span> 24/04/2014
+		<br />
+		<span>Hora: </span>  14:30
+		<br />
+		<span>Profesional: </span> SALAS FUENTELZAS JUAN
+		<br />
+		<span>Especialidad: </span> TRAUMA ADULTO- CIRUGIA RECONSTRUCTIVA
+		<br />
+		<span>Centro Médico: </span> CENTRO MEDICO (3501)
+		<a href="#">Anular Hora</a>
+	</p>
+	<p class="hora">
+		<span>Nombre: </span>MORIAL MARQUEZ CHANAL
+		<br />
+		<span>Código Reserva: </span>  11372248
+		<br />
+		<span>Fecha: </span> 24/04/2014
+		<br />
+		<span>Hora: </span>  14:30
+		<br />
+		<span>Profesional: </span> SALAS FUENTELZAS JUAN
+		<br />
+		<span>Especialidad: </span> TRAUMA ADULTO- CIRUGIA RECONSTRUCTIVA
+		<br />
+		<span>Centro Médico: </span> CENTRO MEDICO (3501)
+		<span class="pasada">Hora Pasada</span>
+	</p>
+</div>
