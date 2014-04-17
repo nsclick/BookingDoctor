2fb923ddaf0ab9b23a0b994655e93720956b99a8
Ricardo Escobar <zherethor@gmail.com>
2014-04-17 12:14:57 -0300

    Página de login
    + detalles para estilos

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -5,30 +5,29 @@
  * Login form
  */
 ?>
-	
-	<h1>Autenticación de usuario</h1>
-
-	<form method="POST" action="<?php echo base_url("login/");?>" role="form">
-	  <div class="form-group">
-	    <label for="rut">R.U.T</label>
-	    <input type="text" class="form-control" id="rut" name="rut" placeholder="Ingrese su RUT">
-	  </div>
-	  <div class="form-group">
-	    <label for="clave">Clave</label>
-	    <input type="password" class="form-control" id="clave" name="clave" placeholder="Clave">
-	  </div>
-	  <button type="submit" class="btn btn-default">Continuar</button> <a href="<?php echo base_url("recuperarclave/");?>">¿Olvidaste tu clave?</a>
-	  <input type="hidden" name="do_login" value="true">
+<div id="wrapper">
+	<h1>Inicio de Sesi&oacute;n</h1>
+	<form method="POST" action="<?php echo base_url("login/"); ?>" role="form">
+		<div class="form-group">
+			<label for="rut">R.U.T</label>
+			<input type="text" class="form-control" id="rut" name="rut" placeholder="Ingrese su RUT">
+		</div>
+		<div class="form-group">
+			<label for="clave">Clave</label>
+			<input type="password" class="form-control" id="clave" name="clave" placeholder="Clave">
+		</div>
+		<div class="form-group final1">
+			
+		</div>
+		<div class="form-group final2">
+			<a href="<?php echo base_url("recuperarclave/"); ?>">Recuperar contrase&ntilde;a</a> |
+			<a href="<?php echo base_url("registro/"); ?>">Registrarse</a>
+			&nbsp;&nbsp;
+			<button type="submit" class="btn btn-default">
+				Continuar
+			</button>
+			<input type="hidden" name="do_login" value="true">
+		</div>
+		<div class="divclear">&nbsp;</div>
 	</form>
-
-	
-	<br>
-	<div class="container-fluid">		  
-		  <div class="row">
-		  	<div class="col-md-12">Si no estás registrado o aún no has establecido tu clave, ingresa aquí: <a href="<?php echo base_url("registro/");?>">Registro</a></div>
-		  </div>	
-		  
-	</div>
-      
-      
-      
+</div>
