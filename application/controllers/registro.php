eb56d5407249253db063992a4a22ced1d4498c8e
Ricardo Escobar <zherethor@gmail.com>
2014-04-21 17:54:36 -0300

    Adicionar Familia + Modificar Datos
    + modificaciones css
    + menores modificaciones en registro
    + menores modificaciones consulta

--- application/controllers/registro.php
+++ application/controllers/registro.php
@@ -1,52 +1,49 @@
 <?php
 if (!defined('BASEPATH'))
-	exit ('No direct script access allowed');
+	exit('No direct script access allowed');
 
 class Registro extends CI_Controller {
 
 	var $title = 'Registro';
 	var $registro_params = array();
 
-	function __construct () {
+	function __construct() {
 		parent::__construct();
-		$this->load->library ( 'Timebooking' );
-		
-		$this->registro_params['phone_prefixes'] = $this->config->item('phone_prefixes');
-		$this->registro_params['medical_services'] = $this->config->item('medical_services');
-		
+		$this -> load -> library('Timebooking');
+
+		$this -> registro_params['phone_prefixes'] = $this -> config -> item('phone_prefixes');
+		$this -> registro_params['medical_services'] = $this -> config -> item('medical_services');
+
 	}
-		
+
 	public function index() {
-		$this->render();
+		$this -> render();
 	}
-	
-	private function render($page = 'registro'){
-		$this->template->write('title', $this->title);
 
-		$this->template->add_js('assets/js/moment-2.4.0.js');
-		$this->template->add_js('assets/third_party/bootstrap/js/bootstrap-datetimepicker.min.js');
-		$this->template->add_js('assets/js/jquery.validationEngine.js');
-		$this->template->add_js('assets/js/jquery.validationEngine-es.js');
-		$this->template->add_js('assets/js/jquery.Rut.js');
-		$this->template->add_js('assets/third_party/bootstrap/js/bootstrap3-typeahead.js');
-		$this->template->add_js('assets/js/jquery.form.min.js');
-		$this->template->add_js('assets/js/registro.js');
-		
-		$this->template->add_css('assets/css/validationEngine.jquery.css');
-		$this->template->add_css('assets/third_party/bootstrap/css/bootstrap-datetimepicker.min.css');
-		
-		
-		$this->template->write_view('header', 'templates/header', $this->registro_params, TRUE);
-		
-		$this->template->write_view('content', "pages/$page", $this->registro_params, TRUE);
+	private function render($page = 'registro') {
+		$this -> template -> write('title', $this -> title);
 
-		$this->template->render();	
+		$this -> template -> add_js('assets/js/moment-2.4.0.js');
+		$this -> template -> add_js('assets/third_party/bootstrap/js/bootstrap-datetimepicker.min.js');
+		$this -> template -> add_js('assets/js/jquery.validationEngine.js');
+		$this -> template -> add_js('assets/js/jquery.validationEngine-es.js');
+		$this -> template -> add_js('assets/js/jquery.Rut.js');
+		$this -> template -> add_js('assets/third_party/bootstrap/js/bootstrap3-typeahead.js');
+		$this -> template -> add_js('assets/js/jquery.form.min.js');
+		$this -> template -> add_js('assets/js/registro.js');
+
+		$this -> template -> add_css('assets/css/validationEngine.jquery.css');
+		$this -> template -> add_css('assets/third_party/bootstrap/css/bootstrap-datetimepicker.min.css');
+
+		$this -> template -> write_view('header', 'templates/header', $this -> registro_params, TRUE);
+
+		$this -> template -> write_view('content', "pages/$page", $this -> registro_params, TRUE);
+
+		$this -> template -> render();
 	}
-	
-	
-	
-	public function guardar(){
-		
+
+	public function guardar() {
+
 		/* Para registrar las opciones de mensajeria usar el metodo del web service
 		 * WM_ActualizaOpcionesMensajeria donde:
 		 * (VIa de confirmación) Op_SmsEmail dede ser:
@@ -54,43 +51,62 @@ class Registro extends CI_Controller {
 		 * 2^  => Sms
 		 * ^3  => Email
 		 * ^   => No quiere ser informado
-		 * 
-		 * Recibe info davila? 
+		 *
+		 * Recibe info davila?
 		 * Op_InfoClinica:
 		 * N => No
 		 * S => Si*/
-		
+
 		//Define the validation rules
-		$validation_rules = array(
-			
-		);
-		
+		$validation_rules = array();
+
 		//To prvent attacks, clean the input
 		$data = array();
-		foreach($_POST as $f => $v){
-			
-			if($f == 'Email_Paciente-confirma' || $f == 'Clave_Usuario-confirma' || $f == 'Comuna_Paciente-label')
+		foreach ($_POST as $f => $v) {
+
+			if ($f == 'Email_Paciente-confirma' || $f == 'Clave_Usuario-confirma' || $f == 'Comuna_Paciente-label')
 				continue;
-			
-			if($f == 'Rut_Paciente'){
-				$v = str_replace('.', '', $this->input->post($f, TRUE));
+
+			if ($f == 'Rut_Paciente') {
+				$v = str_replace('.', '', $this -> input -> post($f, TRUE));
 				$rut = explode('-', $v);
 				$data[$f] = $rut[0];
 				$data['Dv_Paciente'] = $rut[1];
 			} else {
-				$data[$f] = $this->input->post($f, TRUE);
+				$data[$f] = $this -> input -> post($f, TRUE);
 			}
-			
+
 		}
-		
-		$this->load->model('Patient_model', 'patient');
-		
-		$result = $this->patient->create($data);
-		if(!$result){
-			echo json_encode( array('state' => false, 'message' => $this->patient->getError() ) );
+
+		$this -> load -> model('Patient_model', 'patient');
+
+		$result = $this -> patient -> create($data);
+		if (!$result) {
+			echo json_encode(array('state' => false, 'message' => $this -> patient -> getError()));
 			die();
 		}
-		
-		die(json_encode( array('state' => true, 'message' => '' ) ));
+
+		die(json_encode(array('state' => true, 'message' => '')));
 	}
+
+	public function modificardatos() {
+		$this -> template -> write('title', $this -> title);
+		$this -> template -> add_css('assets/vendor/calendarjs/css/calendar.min.css');
+		$this -> template -> add_js('assets/vendor/underscore/underscore-min.js');
+
+		$this -> template -> write_view('header', 'templates/header');
+		$this -> template -> write_view('content', 'pages/modificardatos');
+		$this -> template -> render();
+	}
+	
+	public function adicionafamilia() {
+		$this -> template -> write('title', $this -> title);
+		$this -> template -> add_css('assets/vendor/calendarjs/css/calendar.min.css');
+		$this -> template -> add_js('assets/vendor/underscore/underscore-min.js');
+
+		$this -> template -> write_view('header', 'templates/header');
+		$this -> template -> write_view('content', 'pages/adicionafamilia');
+		$this -> template -> render();
+	}
+
 }
